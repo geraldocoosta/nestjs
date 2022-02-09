0fabd2a25bf434f671dd645b9c914ada7cf383f8
Geraldo Costa <geraldocoosta@gmail.com>
2022-02-09 15:51:39 -0300

    enviando mensagem para topico no kafka

--- src/mail-list/send-mail-tweets.job.ts
+++ src/mail-list/send-mail-tweets.job.ts
@@ -1,15 +1,38 @@
 import { Job } from 'bull';
 import { Process, Processor } from '@nestjs/bull';
 import { MailListService } from './mail-list.service';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { ConfigService } from '@nestjs/config';
+import { Inject } from '@nestjs/common';
 
 @Processor('emails')
 export class SendMailTweetsJob {
-  constructor(private mailListService: MailListService) {}
+  constructor(
+    private mailListService: MailListService,
+    @Inject('KAFKA_PRODUCER')
+    private kafkaProducer: Producer,
+    private configService: ConfigService,
+  ) {}
 
   @Process()
   async handle(job: Job) {
     const mailList = await this.mailListService.findOne();
+    const link = this.configService.get('NEST_HOST');
+    const result = await this.kafkaProducer.send({
+      topic: 'emails',
+      messages: [
+        {
+          key: 'emails',
+          value: JSON.stringify({
+            emails: mailList.emails,
+            subject: 'Novos tweets encontrados',
+            body: `Acesse o link <a href='${link}/tweets'>clicando aqui</a>`,
+          }),
+        },
+      ],
+    });
+    console.log(result);
     console.log(`emails retornados: ${mailList}`);
-    console.log('kafka para enviar a mensagem');
+    console.log('enviou mensagem para outro microserviço');
   }
 }
